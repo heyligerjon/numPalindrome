5538c60a06e00fd7d02caa720971c68874c7bf91
blkmoose <94092151+blkmoose@users.noreply.github.com>
2022-06-06 22:47:25 -0400

    Removed comments

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,27 +1,3 @@
-/*
-6* Palindrome.java
-* Author: Jon Isaacs-Heyliger
-* Submission Date: 2/26/2016
-*
-* Purpose: This program is designed to determine if a number is a palindrome and if 
-* it is, find the the value of the sum of the numbers from 1 to the number
-* 
-* Statement of Academic Honesty:
-*
-* The following code represents my own work. I have neither
-* received nor given inappropriate assistance. I have not copied
-* or modified code from any source other than the course webpage
-* or the course textbook. I recognize that any unauthorized
-* assistance or plagiarism will be handled in accordance with
-* the University of Georgia's Academic Honesty Policy and the
-* policies of this course. I recognize that my work is based
-* on an assignment created by the Department of Computer
-* Science at the University of Georgia. Any publishing
-* or posting of source code for this project is strictly
-* prohibited unless you have written consent from the Department
-* of Computer Science at the University of Georgia.
-*/
-
 import java.util.Scanner;
 
 public class Palindrome {
